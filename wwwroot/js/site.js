964959761d99ac4c6e75b0a835f32ba0a9d1898c
abdullahercin <abdullahercin@gmail.com>
2024-07-09 14:28:44 +0300

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,4 @@
+﻿window.updateRangeBackground = (selector, percentage) => {
+    const range = document.querySelector(selector);
+    range.style.background = `linear-gradient(to right, #e53e3e 0%, #e53e3e ${percentage}%, #f3f4f6 ${percentage}%, #f3f4f6 100%)`;
+};
